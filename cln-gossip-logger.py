53bb27803cb149d617b2974c64d817f585d2c154
Simon <simon@honc.online>
2022-10-01 21:56:47 +0200

    try printing writes

--- cln-gossip-logger.py
+++ cln-gossip-logger.py
@@ -21,6 +21,7 @@ root.addHandler(handler)
 @plugin.hook('db_write')
 def on_db_write(writes, data_version, plugin, **kwargs):
     plugin.log("hello im in the on_db_write hook, writes array size: {size}".format(size=len(writes)),level="warn")
+    plugin.log("print the writes: {writes}".format(writes=writes),level="warn")
     if True:
         return {"result": "continue"}
     else:
